bbdfbba1ac444033ecbac11b1cb28a9ecca15654
teddy Andrianto <andrianto.teddy@gmail.com>
2019-03-20 21:53:43 -0700

    Login

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -3,23 +3,39 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Landing extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+ 	{
+ 		date_default_timezone_set('Asia/Jakarta');
+ 		parent::__construct();
+ 		$this->load->model('Model_login');
+	  }
+	  
 	public function index()
 	{
-		$this->load->view('page_login');
+		if(isset($_SESSION['login'])){
+			redirect("landing/autentifikasi_login");	
+		}else{
+			$this->load->view('page_login');
+		}
+	}
+
+	public function autentifikasi_login()
+	{
+		$this->Model_login->verivikasi_login();
+		if(isset($_SESSION['login'])){
+			if($_SESSION['login']['role']==1){
+				echo "login role 1";	
+			}elseif($_SESSION['login']['role']==2){
+				echo "login role 2";	
+			}
+		}else{
+			echo "login Gagal";
+		}
+	}
+
+	public function logout()
+	{
+		session_destroy();
+		redirect("landing");
 	}
 }
--- application/models/Model_Login.php
+++ application/models/Model_Login.php
@@ -1,5 +1,5 @@
 <?php 
-class Model_Login extends CI_Model {
+class Model_login extends CI_Model {
 	
 	public function verivikasi_login() {
 		$username = $this->input->POST('username', TRUE);
@@ -9,8 +9,8 @@ class Model_Login extends CI_Model {
 			return false;
 		}else{
 			$data = $query->row();
-			$_SESSION['login'] = array('id_user'=>$data->id_user,'username'=>$data->username,'nama_user'=>$data->nama_user,"password"=>$data->password,"status"=>$data->status,"foto"=>$data->foto,"telpon"=>$data->telpon);
-			return $_SESSION['login'];
+			$_SESSION['login'] = array('id_user'=>$data->id_user,'username'=>$data->username,'nama'=>$data->nama,"password"=>$data->password,"role"=>$data->role,"foto"=>$data->foto,"no_telpon"=>$data->no_telpon,"alamat"=>$data->alamat);
+			return true;
 		}
 	}
 }
